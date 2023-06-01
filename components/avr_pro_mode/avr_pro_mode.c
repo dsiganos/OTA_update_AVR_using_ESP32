2107334a2a262c623ee740b3e59730b140122fb2
Dimitrios Siganos <dimitris@siganos.org>
2023-06-01 19:59:34 +0100

    Add source_clk = UART_SCLK_DEFAULT to uart config

--- components/avr_pro_mode/avr_pro_mode.c
+++ components/avr_pro_mode/avr_pro_mode.c
@@ -17,7 +17,9 @@ void initUART(void)
         .data_bits = UART_DATA_8_BITS,
         .parity = UART_PARITY_DISABLE,
         .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE};
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT
+    };
     uart_param_config(UART_NUM_1, &uart_config);
     uart_set_pin(UART_NUM_1, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
     uart_driver_install(UART_NUM_1, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
